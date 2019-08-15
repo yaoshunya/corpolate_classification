77f770fc6de58ea1aa6c7738a46c15a63b31496d
yaoshunya <newton800pola@gmail.com>
2019-08-15 20:12:40 +0900

    add some file

---
+++ code/preprocess.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pdb
+import numpy as np
+import tensorflow as tf
+
+if __name__ ==  '__main__':
+
+    df = pd.read_csv("../data/corporation_sample.csv",encoding="shift_jis")
+    pdb.set_trace()
