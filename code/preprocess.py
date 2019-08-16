48bd6adb8f6dea2cda6d7b1b535bc878289218c2
yaoshunya <newton800pola@gmail.com>
2019-08-16 17:39:17 +0900

    add code

--- code/preprocess.py
+++ code/preprocess.py
@@ -2,8 +2,19 @@ import pandas as pd
 import pdb
 import numpy as np
 import tensorflow as tf
+import MeCab
+from gensim.models import word2vec
 
 if __name__ ==  '__main__':
 
-    df = pd.read_csv("../data/corporation_sample.csv",encoding="shift_jis")
+    df = pd.read_csv("../data/corporation_sample.csv")
+
+    mt = MeCab.Tagger('')
+    mt.parse('') 
+  	 
+    model = word2vec.Word2Vec.load("../data/wiki.model")
+
+    title = df['title']
+    description = df['description']
+    y = df['class']
     pdb.set_trace()
